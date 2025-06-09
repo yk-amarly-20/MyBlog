30c506f5ff9fd47785cda411101c778927f766d7
yk-amarly-20 <y.k.83600260@icloud.com>
2025-06-09 15:29:47 +0900

    Updates

--- src/modules/LayoutEffect.ts
+++
@@ -1,3 +0,0 @@
-import { useLayoutEffect, useEffect } from "react";
-
-export const LayoutEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect;
--- src/modules/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  siteTitle: "kojikoji's blog",
-  siteUrl: "",
-  social: {
-    twitter: "@yk_amarly_20",
-  },
-  excerptLength: 160,
-} as const;
