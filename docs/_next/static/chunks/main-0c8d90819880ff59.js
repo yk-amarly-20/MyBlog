e9d70630c0c3e0b2336f880e16b52bf97630dc1c
yk-amarly-20 <y.k.83600260@icloud.com>
2025-06-09 17:07:47 +0900

    Deploy via docs

--- docs/_next/static/chunks/main-0c8d90819880ff59.js
+++ docs/_next/static/chunks/main-0c8d90819880ff59.js
@@ -1186,7 +1186,7 @@
                         value: {
                           deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
                           imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-                          path: "/MyBlog/_next/image/",
+                          path: "./_next/image/",
                           loader: "akamai",
                           dangerouslyAllowSVG: !1,
                           unoptimized: !0,
